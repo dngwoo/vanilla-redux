7df58d9efb2a1bb75b76f1e8374132d0de1bfd57
dngwoo <akfmzk84@naver.com>
2020-09-05 21:58:28 +0900

    Detail Screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,25 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+const Detail = ({ toDo }) => {
+  console.log(toDo); // {text: "22", id: 1599309858660}
+  return (
+    <>
+      <h1>Content : {toDo?.text}</h1>
+      <div>Create at : {toDo?.id}</div>
+    </>
+  );
+};
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  return {
+    // find() 메서드는 주어진 판별 함수를 만족하는 첫 번째 요소의 값을 반환
+    toDo: state.find((v) => v.id === parseInt(id)),
+  };
+}
+export default connect(mapStateToProps)(Detail);
